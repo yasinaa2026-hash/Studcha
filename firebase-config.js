c68cb690cecd18cb5c240260b96bce16a4b0069f
hash <yasinaa2026@gmail.com>
2026-09-13 17:26:46 +0800

    chore: add Firebase config template

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Web App configuration.
+// These values come from your Firebase project settings.
+// The browser config is not a server secret; protect the data with Firebase Security Rules.
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
